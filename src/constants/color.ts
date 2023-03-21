bda5c9259d11ae684ab1b912c97e4cddae85101b
dan <dany.condurari@gmail.com>
2023-03-21 23:11:30 +0200

    new change

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -2,6 +2,6 @@ export const color = {
   black: "#403B3B",
   black_opacity: "rgba(19, 29, 59, .25)",
   white_gray: "#E7EAE8",
-  bg_color: "rgb(255,224,178)",
+  bg_color: "#F2F2F2",
   footer_color: "rgb(195,168,123)",
 };
